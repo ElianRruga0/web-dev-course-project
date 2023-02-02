2cc12fe636eacc07db21093356b88d0524c5a549
ElianRruga0 <elianrruga@gmail.com>
2023-02-02 14:12:02 +0100

    auth midd fix

--- app/Http/Controllers/ActivityTypeController.php
+++ app/Http/Controllers/ActivityTypeController.php
@@ -9,7 +9,7 @@ class ActivityTypeController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        // $this->middleware('auth:api');
     }
 
     public function index()
